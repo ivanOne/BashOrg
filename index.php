e20ff5e6997b77acb3505d59cfddaf75528a26c9
IvanOne <turnthepage41@gmail.com>
2014-08-26 11:55:44 +0400

    Лишние функции убраны

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
 require_once("lib/Models.php");
-error_reporting(-1);
 $var = new Models();
 $var->select('one')->execute()->fetch();
-var_dump($var);
--- lib/Models.php
+++ lib/Models.php
@@ -1,7 +1,6 @@
 <?php
 
-class DB
-{
+class DB{
     private  $connection;
     protected static $instance;
 
@@ -25,82 +24,95 @@ class DB
 
 }
 
-class Models
-{
+class Models{
     protected $query;
 
-    /*метод для типа запроса SELECT,
-    $field - строка с полями через запятую, если в параметре ничего не указать
-    то выберутся все поля подобно запросу SELECT *
-    $from - строка с таблицей */
-    public function select($from,$field = NULL)
-    {
-        if(is_string($field))
+    /**
+     * Метод для типа запроса SELECT,
+     * $field - строка с полями через запятую, если в параметре ничего не указать
+     * то выберутся все поля подобно запросу SELECT *
+     * $from - строка с таблицей
+    */
+    public function select($from,$field = NULL){
+        if(is_string($field)){
             $this->query = "SELECT ".$field." FROM ".$from." ";
-        elseif($field === NULL)
+        }
+        elseif($field === NULL){
             $this->query = "SELECT * FROM ".$from." ";
+        }
         return $this;
     }
 
-    /*Метод задает условие запроса
-    $condition строка с условием запроса*/
-    public function where($condition)
-    {
+    /**
+     * Метод задает условие запроса
+     * $condition строка с условием запроса
+    */
+    public function where($condition){
         $this->query .= "WHERE ".$condition." ";
         return $this;
     }
 
-    /*Сортировка запрса
-    $field строка с указанием поля для сортировки
-    $param Строка с параметром сортировки ASC/DESC */
-    protected function orderBy($field,$param)
-    {
+    /**
+     * Сортировка запрса
+     * $field строка с указанием поля для сортировки
+     * $param Строка с параметром сортировки ASC/DESC
+    */
+    protected function orderBy($field,$param){
         $this->query .= "ORDER BY ".$field." ".$param." ";
         return $this;
     }
 
-    /*Дополнительная сортировка запрса
-    $field строка с указанием поля для сортировки
-    $param Строка с параметром сортировки ASC/DESC */
-    public function addOrderBy($field,$param)
-    {
+    /**
+     * Дополнительная сортировка запрса
+     * $field строка с указанием поля для сортировки
+     * $param Строка с параметром сортировки ASC/DESC
+    */
+    public function addOrderBy($field,$param){
         $this->query .= ",".$field." ".$param." ";
         return $this;
     }
 
-    /*Метод определяет лимит запроса*/
-    public function limit($offset,$count = NULL)
-    {
+    /**
+     * Метод определяет лимит запроса
+    */
+    public function limit($offset,$count = NULL){
         if($count === NULL)
             $this->query .= "LIMIT ".$offset." ";
         else
             $this->query .= "LIMIT ".$offset.",".$count."";
         return $this;
     }
-    //Выполнение запроса
-    public function execute()
-    {
+
+    /**
+     * Выполнение запроса
+    */
+    public function execute(){
         $this->query = DB::run()->getConn()->prepare($this->query);
         $this->query->execute();
         return $this;
     }
 
-    //Извлечение результата
+    /**
+     * Извлечение результата
+    */
     public function fetch()
     {
         $result = $this->query->fetch(PDO::FETCH_ASSOC);
         return $result;
     }
 
-    //Извлечение множественного результата
-    public function fetchAll()
-    {
+    /**
+     * Извлечение множественного результата
+    */
+    public function fetchAll(){
         $result = $this->query->fetchAll(PDO::FETCH_ASSOC);
         return $result;
     }
-    //Число затронутых строк
-    public function count()
-    {
+
+    /**
+     * Число затронутых строк
+    */
+    public function count(){
         $count = $this->query->columnCount();
         return $count;
     }
